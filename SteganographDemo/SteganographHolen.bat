26db8a3093240cd440e0376924151affa734289d
cpl <christoph.platen@n-design.de>
2020-09-04 08:08:24 +0200

    Batch-Dateien angepasst

--- SteganographDemo/SteganographHolen.bat
+++ SteganographDemo/SteganographHolen.bat
@@ -1,2 +1,2 @@
-java -jar ./Steganograph-1.1.1.jar --holen --dateiVerteilregel verteilregel --dateiNutzdaten nutzdatenNeu.txt --dateiQuelle testbild1Versteck.png
+java -jar ./Steganograph-1.2.0.jar --holen --dateiVerteilregel verteilregel --dateiNutzdaten nutzdatenNeu.txt --dateiQuelle testbild1Versteck.png
 pause
--- SteganographDemo/SteganographVerstecken.bat
+++ SteganographDemo/SteganographVerstecken.bat
@@ -1,2 +1,2 @@
-java -jar ./Steganograph-1.1.1.jar --verstecken --dateiVerteilregel verteilregel --dateiNutzdaten nutzdaten.txt --dateiQuelle testbild1.png --dateiZiel testbild1Versteck.png
+java -jar ./Steganograph-1.2.0.jar --verstecken --dateiVerteilregel verteilregel --dateiNutzdaten nutzdaten.txt --dateiQuelle testbild1.png --dateiZiel testbild1Versteck.png
 pause
