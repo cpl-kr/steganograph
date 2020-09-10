2b207eabbf606107ef41cb6c993e7f14b52841a1
chris <chris@MININT-2US8JLD>
2020-09-10 18:56:08 +0200

    Testfall zur Ausgabe von Zufallszahlen

--- src/test/java/de/platen/zufallszahlengenerator/ZufallszahlengeneratorTest.java
+++ src/test/java/de/platen/zufallszahlengenerator/ZufallszahlengeneratorTest.java
@@ -4,11 +4,32 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Set;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ZufallszahlengeneratorTest {
+
+    @Test
+    @Ignore
+    public void testErzeugeZufallszahlenmenge() {
+        Set<Integer> zufallszahlen = Zufallszahlengenerator.erzeugeZufallszahlenmenge(new AnzahlZufallszahlen(500),
+                new BereichVon(10), new BereichBis(5001));
+        assertEquals(500, zufallszahlen.size());
+        List<Integer> zahlen = new ArrayList<>(zufallszahlen);
+        Collections.sort(zahlen);
+        for (Integer zufallszahl : zahlen) {
+            System.out.println("Zufallszahl: " + zufallszahl);
+            if (!((zufallszahl >= 10) && (zufallszahl < 5001))) {
+                System.out.println("Zufallszahl ist nicht im Bereich: " + zufallszahl);
+            }
+            assertTrue((zufallszahl >= 10) && (zufallszahl < 5001));
+        }
+    }
 
     @Test
     public void testErzeugeZufallszahlenmenge1() {
