3eb6b8a1bd9c1e76a90f07e2046834bc687ab187
cpl-kr <chris@MININT-2US8JLD>
2020-09-11 15:55:04 +0200

    Weitere Testbilder

--- src/test/java/de/platen/steganograph/BildTest.java
+++ src/test/java/de/platen/steganograph/BildTest.java
@@ -13,15 +13,24 @@ public class BildTest {
     @Test
     @Ignore
     public void testBild() throws IOException {
-        erzeugeTestbild("src/test/resources/testbild1.png", 0xFF0000FF); // Blau
-        erzeugeTestbild("src/test/resources/testbild2.png", 0xFF00FF00); // Grün
-        erzeugeTestbild("src/test/resources/testbild3.png", 0xFFFF0000); // Rot
-        erzeugeTestbild("src/test/resources/testbild4.png", 0xFF000000); // Schwarz
-        erzeugeTestbild("src/test/resources/testbild5.png", 0xFFFFFFFF); // Weiß
+        erzeugeTestbild("src/test/resources/BildBlau4ByteABGR.png", 0xFF0000FF, BufferedImage.TYPE_4BYTE_ABGR); // Blau
+        erzeugeTestbild("src/test/resources/BildGruen4ByteABGR.png", 0xFF00FF00, BufferedImage.TYPE_4BYTE_ABGR); // Grün
+        erzeugeTestbild("src/test/resources/BildRot4ByteABGR.png", 0xFFFF0000, BufferedImage.TYPE_4BYTE_ABGR); // Rot
+        erzeugeTestbild("src/test/resources/BildSchwarz4ByteABGR.png", 0xFF000000, BufferedImage.TYPE_4BYTE_ABGR); // Schwarz
+        erzeugeTestbild("src/test/resources/BildWeiss4ByteABGR.png", 0xFFFFFFFF, BufferedImage.TYPE_4BYTE_ABGR); // Weiß
+
+        erzeugeTestbild("src/test/resources/BildBlau3ByteBGR.png", 0x0000FF, BufferedImage.TYPE_3BYTE_BGR); // Blau
+        erzeugeTestbild("src/test/resources/BildGruen3ByteBGR.png", 0x00FF00, BufferedImage.TYPE_3BYTE_BGR); // Grün
+        erzeugeTestbild("src/test/resources/BildRot3ByteBGR.png", 0xFF0000, BufferedImage.TYPE_3BYTE_BGR); // Rot
+        erzeugeTestbild("src/test/resources/BildSchwarz3ByteBGR.png", 0x000000, BufferedImage.TYPE_3BYTE_BGR); // Schwarz
+        erzeugeTestbild("src/test/resources/BildWeiss3ByteBGR.png", 0xFFFFFF, BufferedImage.TYPE_3BYTE_BGR); // Weiß
+
+        erzeugeTestbild("src/test/resources/BildSchwarzByteGray.png", 0x00, BufferedImage.TYPE_3BYTE_BGR); // Schwarz
+        erzeugeTestbild("src/test/resources/BildWeissByteGray.png", 0xFF, BufferedImage.TYPE_3BYTE_BGR); // Weiß
     }
 
-    private static void erzeugeTestbild(String dateiname, int farbe) throws IOException {
-        final BufferedImage bufferedImage = new BufferedImage(400, 400, BufferedImage.TYPE_4BYTE_ABGR);
+    private static void erzeugeTestbild(String dateiname, int farbe, int typ) throws IOException {
+        final BufferedImage bufferedImage = new BufferedImage(400, 400, typ);
         for (int x = 0; x < 400; x++) {
             for (int y = 0; y < 400; y++) {
                 bufferedImage.setRGB(x, y, farbe);
