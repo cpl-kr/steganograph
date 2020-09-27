3a4f7fbdb5a0161bcf18a9ec3411aa121832f5b6
cpl-kr <christoph.platen@outlook.de>
2020-09-27 14:05:32 +0200

    In Stzartscripten die Version angepasst

--- SteganographDemo/SteganographVerstecken.bat
+++ SteganographDemo/SteganographVerstecken.bat
@@ -1,2 +1,2 @@
-java -jar ./Steganograph-1.2.1.jar --verstecken --dateiVerteilregel verteilregel --dateiNutzdaten nutzdaten.txt --dateiQuelle testbild1.png --dateiZiel testbild1Versteck.png --verrauschen ohne
+java -jar ./Steganograph-1.3.0.jar --verstecken --dateiVerteilregel verteilregel --dateiNutzdaten nutzdaten.txt --dateiQuelle testbild1.png --dateiZiel testbild1Versteck.png --verrauschen ohne
 pause
