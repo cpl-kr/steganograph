88ff78cd72a7018e82f8a8ef11401a0f302157e7
cpl-kr <chris@MININT-2US8JLD>
2020-10-03 10:32:21 +0200

    Demo mit anderem Testbild

--- SteganographDemo/SteganographHolen.bat
+++ SteganographDemo/SteganographHolen.bat
@@ -1,2 +1,2 @@
-java -jar ./Steganograph-1.3.0.jar --holen --dateiVerteilregel verteilregel --dateiNutzdaten nutzdatenNeu.txt --dateiQuelle testbild1Versteck.png
+java -jar ./Steganograph-1.3.0.jar --holen --dateiVerteilregel verteilregel --dateiNutzdaten nutzdatenNeu.txt --dateiQuelle TestbildVersteck.png
 pause
