9b3f5c977c42aadada34e4fe97c35fc8861b13ff
cpl-kr <chris@MININT-2US8JLD>
2020-10-05 22:30:09 +0200

    Aktion für Holen aus Audiodatei

---
+++ src/main/java/de/platen/steganograph/AktionHolenAusAudio.java
@@ -0,0 +1,9 @@
+package de.platen.steganograph;
+
+public class AktionHolenAusAudio {
+
+    public static void holeNutzdatenAusAudio(String dateinameVerteilregel, String dateinameQuelle,
+            String dateinameNutzdaten) {
+        // TODO
+    }
+}
