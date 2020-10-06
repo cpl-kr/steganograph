0db00a1685e2ac053e18811975cb3d875bb8f7fd
cpl-kr <chris@MININT-2US8JLD>
2020-10-06 17:53:18 +0200

    Anpassung Demoversion

--- SteganographDemo/SteganographVerstecken.bat
+++
@@ -1,2 +0,0 @@
-java -jar ./Steganograph-1.3.0.jar --verstecken --dateiVerteilregel verteilregel --dateiNutzdaten nutzdaten.txt --dateiQuelle Testbild.png --dateiZiel TestbildVersteck.png --verrauschen ohne
-pause
