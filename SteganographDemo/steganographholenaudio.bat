4a5ce540057e148b87fbb2de3c0b7c4266f02c09
Christoph Platen <christoph.platen@outlook.de>
2023-04-02 18:25:23 +0200

    Neue Version

--- SteganographDemo/steganographholenaudio.bat
+++ SteganographDemo/steganographholenaudio.bat
@@ -1,2 +1,2 @@
-java -jar ./Steganograph-2.1.0.jar --holen --dateiVerteilregel verteilregelaudio --dateiNutzdaten nutzdatenneu.txt --dateiQuelle testaudioversteck.wav --entschluesselungsdatei private1.pgp --passwort passwort
+java -jar ./Steganograph-2.1.1.jar --holen --dateiVerteilregel verteilregelaudio --dateiNutzdaten nutzdatenneu.txt --dateiQuelle testaudioversteck.wav --entschluesselungsdatei private1.pgp --passwort passwort
 pause
--- SteganographDemo/steganographversteckenbild.bat
+++ SteganographDemo/steganographversteckenbild.bat
@@ -1,2 +1,2 @@
-java -jar ./Steganograph-2.1.0.jar --verstecken --dateiVerteilregel verteilregelbild --dateiNutzdaten nutzdaten.txt --dateiQuelle testbild.png --dateiZiel testbildversteck.png --verrauschen ohne --entschluesselungsdatei private1.pgp --passwort passwort
+java -jar ./Steganograph-2.1.1.jar --verstecken --dateiVerteilregel verteilregelbild --dateiNutzdaten nutzdaten.txt --dateiQuelle testbild.png --dateiZiel testbildversteck.png --verrauschen ohne --entschluesselungsdatei private1.pgp --passwort passwort
 pause
